10cfcefbbb9d8e911d488a17cb3dc5e3fc7d141b
Jell <jeanlouis@Jell.local>
2010-08-17 09:36:58 +0200

    Adapted to new SL API

--- Rails/app/models/provider/storstockholms_lokaltrafik.rb
+++ Rails/app/models/provider/storstockholms_lokaltrafik.rb
@@ -24,162 +24,158 @@ module Provider::StorstockholmsLokaltrafik
   end
 
   def get_forecast_url(poi_id)
-    url = "http://realtid.sl.se/?epslanguage=SV&WbSgnMdl="+poi_id+"-U3Rv-_-1-_-1-1"
+    #url = "http://realtid.sl.se/?epslanguage=SV&WbSgnMdl="+poi_id+"-U3Rv-_-1-_-1-1"
+    url = "http://www1.sl.se/realtidws/RealTimeService.asmx/GetDpsDepartures?siteId=" + poi_id + "&timeWindow=30"
   end
   
   def parse_forecast(response, poi_id, poi_name)
-    html_tree = Nokogiri::HTML(response)
+    #html_tree = Nokogiri::HTML(response)
+    
+    xml_tree = Nokogiri::XML.parse(response, nil, 'UTF-8')
 
     lines = []
 
-    # find Bus Forecasts
-    bus_forecast = html_tree.xpath('//div[@class="TrafficTypeItem"]')
-    bus_forecast.each do |element|
-      stop_name = element.css('div h3').text.split(", ")[1]
-      if(stop_name == poi_name)
-        departure_list = element.css('div[@class="Departure"]') + element.css('div[@class="DepartureAlternating"]')
-        departure_list.each do |departure|
-          next_trip_text = departure.css('span[@class="DisplayTime"]').text.squeeze(" ").strip
-          if next_trip_text =~ /[Nn]u/
-            next_trip = "0"
-          else
-            next_trip = departure.css('span[@class="DisplayTime"]').text.squeeze(" ").strip.scan(/\d+\smin/).first
-          end
-          if next_trip != nil
-            attributes = {}
-            attributes[:line_number]       = departure.css('span[@class="LineDesignation"]').text.squeeze(" ").strip
-            attributes[:color]             = "#FFFFFF"
-            attributes[:background_color]  = "#BB0000"
-            attributes[:destination]       = departure.css('span[@class="DestinationName"]').text.squeeze(" ").strip
-            attributes[:next_trip]         = next_trip.scan(/\d+/).first
-            attributes[:next_handicap]        = false
-            attributes[:next_low_floor]       = false
-            attributes[:next_next_trip]       = ""
-            attributes[:next_next_handicap]        = false
-            attributes[:next_next_low_floor]       = false
+    buses = xml_tree.css("Buses").css("DpsBus")
+    metros = xml_tree.css("Metros").css("DpsMetro")
+    trains = xml_tree.css("Trains").css("DpsTrain")
+    trams = xml_tree.css("Trams").css("DpsTram")
+    
+    # find Bus Forecasts #############################################
+    bus_list = {}
+    buses.each do |bus|
+      line = bus.css("LineNumber").text
+      destination = bus.css("Destination").text
+      bus_list[{:line => line, :destination => destination}] ||= []
+      bus_list[{:line => line, :destination => destination}] |= [format_time(bus.css("DisplayTime").text)]
+    end
+    
+    bus_list.each do |id, departures|
+      attributes = {}
+      attributes[:line_number]       = id[:line]
+      attributes[:color]             = "#FFFFFF"
+      attributes[:background_color]  = "#BB0000"
+      attributes[:destination]       = id[:destination]
+      attributes[:next_trip]         = "#{departures[0]}"
+      attributes[:next_handicap]        = false
+      attributes[:next_low_floor]       = false
+      attributes[:next_next_trip]       = "#{departures[1]}"
+      attributes[:next_next_handicap]        = false
+      attributes[:next_next_low_floor]       = false
 
-            lines << [attributes[:line_number], attributes]
-          end
-        end
-      end
+      lines << [attributes[:line_number], attributes]
     end
 
-    # find Tram Forecasts
-    tram_forecast = html_tree.xpath('//div[@class="TPIHolder"]')
-    is_correct_stop = false
-    tram_forecast.css('span[@class="Row1"]').each do |element|
-      is_correct_stop |= (element.text == poi_name)
+
+    # find Metro Forecasts #############################################
+    colors = {"10" => "#0080FF",
+          "11" => "#0080FF",
+          "13" => "#FF0000",
+          "14" => "#FF0000",
+          "17" => "#00BB00",
+          "18" => "#00BB00",
+          "19" => "#00BB00"}
+          
+    metro_list = {}
+    metros.each do |metro|
+      line = metro.css("LineNumber").text
+      destination = metro.css("Destination").text
+      metro_list[{:line => line, :destination => destination}] ||= []
+      metro_list[{:line => line, :destination => destination}] |= [format_time(metro.css("DisplayTime").text)]
     end
-    if is_correct_stop
-      colors = {"T10" => "#0080FF",
-                "T11" => "#0080FF",
-                "T13" => "#FF0000",
-                "T14" => "#FF0000",
-                "T17" => "#00BB00",
-                "T18" => "#00BB00",
-                "T19" => "#00BB00"}
-                
-      departures = tram_forecast.css('div[@class="Row2"]')
-      tram_regexp = /\d+[a-zA-ZöäåÖÄÅ\s\.]+\d+\smin/
-      
-      list_tmp = []
-      departures.each do |departure|
-        matches = departure.text.scan(tram_regexp)
-        if matches != nil
-          matches.each do |match|
-            list_tmp << match
-          end
-        end
-      end
-      list_tmp.each do |to_parse|
-        parsed = to_parse.squeeze(" ").split(" ")
-        parsed_size = parsed.count
-        line_number = "T" + parsed[0]
-        next_trip = parsed[parsed_size-2]
-        destination = parsed[1]
-        for i in 2..parsed_size-3
-          destination += " " + parsed[i]
-        end
-        
-        attributes = {}
-        attributes[:line_number]       = line_number
-        attributes[:color]             = "#FFFFFF"
-        attributes[:background_color]  = colors[line_number]
-        attributes[:destination]       = destination
-        attributes[:next_trip]         = next_trip
-        attributes[:next_handicap]        = false
-        attributes[:next_low_floor]       = false
-        attributes[:next_next_trip]    = ""
-        attributes[:next_next_handicap]        = false
-        attributes[:next_next_low_floor]       = false
-        lines << [attributes[:line_number], attributes]
-      end
+
+    metro_list.each do |id, departures|
+      attributes = {}
+      attributes[:line_number]       = "T" + id[:line]
+      attributes[:color]             = "#FFFFFF"
+      attributes[:background_color]  = colors[id[:line]]
+      attributes[:destination]       = id[:destination]
+      attributes[:next_trip]         = "#{departures[0]}"
+      attributes[:next_handicap]        = false
+      attributes[:next_low_floor]       = false
+      attributes[:next_next_trip]       = "#{departures[1]}"
+      attributes[:next_next_handicap]        = false
+      attributes[:next_next_low_floor]       = false
+
+      lines << [attributes[:line_number], attributes]
     end
     
-    # find Pendeltåg
-    pendeltag_forecast = html_tree.xpath('//div[@class="PPIHolder"]')
-    pendeltag_header = pendeltag_forecast.css('div[@class="Header"]').css('h3')
-    
-    is_correct_stop = (pendeltag_header.text =~ /#{poi_name}/)
-    if is_correct_stop
-      Time.zone = "Stockholm"
-      current_hour    = Time.zone.now.hour
-      current_minute  = Time.zone.now.min
-      departure_list  =  pendeltag_forecast.css('div[@class="Departure TrainRow"]') +
-                          pendeltag_forecast.css('div[@class="DepartureAlternating TrainRow"]')
-      departure_list.each do |departure|
-        striped_name = departure.css('span[@class="TrainCell Col2"]').text.scan(/[a-zA-ZöäåÖÄÅ\s]+/)
-        destination = striped_name[0]
-        for i in 1..striped_name.count - 2
-          destination += " " + striped_name[i]
-        end
-        
-        unformated_time = departure.css('span[@class="TrainCell"]').text.scan(/\d\d*:\d\d/)[0]
-        if unformated_time == nil
-          unformated_time = departure.css('span[@class="TrainCell Col1"]').text.scan(/\d\d*:\d\d/)[0]
-        end
-        splited_time = unformated_time.split(":")
-        departure_hour = splited_time[0].to_i
-        departure_minute = splited_time[1].to_i
-        
-        time_num = 60*(departure_hour - current_hour) + (departure_minute - current_minute)
-        if time_num >=0
-          next_trip = time_num.to_s
-        else
-          next_trip = ""
-        end
-        attributes = {}
-        attributes[:line_number]       = "Pendeltåg"
-        attributes[:color]             = "#FFFFFF"
-        attributes[:background_color]  = "#000000"
-        attributes[:destination]       = destination
-        attributes[:next_trip]         = next_trip
-        attributes[:next_handicap]        = false
-        attributes[:next_low_floor]       = false
-        attributes[:next_next_trip]    = ""
-        attributes[:next_next_handicap]        = false
-        attributes[:next_next_low_floor]       = false
-        lines << [attributes[:line_number], attributes]
-      end
-      
+    #find train forecast#############################################
+    train_list = {}
+    trains.each do |train|
+      line = train.css("LineNumber").text
+      destination = train.css("Destination").text
+      train_list[{:line => line, :destination => destination}] ||= []
+      train_list[{:line => line, :destination => destination}] |= [format_time(train.css("DisplayTime").text)]
     end
     
+    train_list.each do |id, departures|
+      attributes = {}
+      attributes[:line_number]       = "P" + id[:line]
+      attributes[:color]             = "#FFFFFF"
+      attributes[:background_color]  = "#000000"
+      attributes[:destination]       = id[:destination]
+      attributes[:next_trip]         = "#{departures[0]}"
+      attributes[:next_handicap]        = false
+      attributes[:next_low_floor]       = false
+      attributes[:next_next_trip]       = "#{departures[1]}"
+      attributes[:next_next_handicap]        = false
+      attributes[:next_next_low_floor]       = false
+
+      lines << [attributes[:line_number], attributes]
+    end
+    
+    #find tram forecast#############################################
+    colors = {"7" => "#FFFFFF",
+              "12" => "#008457",
+              "21" => "#BF2A37",
+              "22" => "#F8981C",
+              "25" => "#691F7E",
+              "26" => "#691F7E",
+              "27" => "#007CC3",
+              "28" => "#007CC3",
+              "29" => "#007CC3"}
+              
+              7 == 7 ? 1 : 2
+    tram_list = {}
+    trams.each do |tram|
+      line = tram.css("LineNumber").text
+      destination = tram.css("Destination").text
+      tram_list[{:line => line, :destination => destination}] ||= []
+      tram_list[{:line => line, :destination => destination}] |= [format_time(tram.css("DisplayTime").text)]
+    end
+
+    tram_list.each do |id, departures|
+      attributes = {}
+      attributes[:line_number]       = "T" + id[:line]
+      attributes[:color]             = (id[:line] == 7 ? "#000000" : "#FFFFFF")
+      attributes[:background_color]  = colors[id[:line]]
+      attributes[:destination]       = id[:destination]
+      attributes[:next_trip]         = "#{departures[0]}"
+      attributes[:next_handicap]        = false
+      attributes[:next_low_floor]       = false
+      attributes[:next_next_trip]       = "#{departures[1]}"
+      attributes[:next_next_handicap]        = false
+      attributes[:next_next_low_floor]       = false
+
+      lines << [attributes[:line_number], attributes]
+    end
     return sort_lines(lines)
   end
   
   private
   
   def fetch_stop_with_id(id)
+    Time.zone = "Stockholm"
     params = {
       "REQ0HafasSearchForw"	=>"1",
-      "REQ0JourneyDate"			=>"25.03.10",
+      "REQ0JourneyDate"			=>Time.zone.now.strftime("%d.%m.%Y"),
       "REQ0JourneyStopsS0A"	=>"255",
       "REQ0JourneyStopsS0G"	=>id,
       "REQ0JourneyStopsSID"	=>"",
       "REQ0JourneyStopsZ0A"	=>"255",
       "REQ0JourneyStopsZ0G"	=>"",
       "REQ0JourneyStopsZID"	=>"",
-      "REQ0JourneyTime"			=>"10:10",
+      "REQ0JourneyTime"			=>Time.zone.now.strftime("%H:%M"),
       "existUnsharpSearch"	=>"yes",
       "ignoreTypeCheck"			=>"yes",
       "queryPageDisplayed"	=>"no",
@@ -219,6 +215,16 @@ module Provider::StorstockholmsLokaltrafik
     attributes[:stop_id] = id
 
     return attributes
+  end
+  
+  def format_time(string)
+    return string.scan(/\d+/).first.to_i if string =~ /\d+ min/
+    return nil unless string =~ /\d+:\d\d/
+    Time.zone = "Stockholm"
+    splited_time = string.split(":")
+    hours = splited_time[0].to_i
+    minutes = splited_time[1].to_i
+    60*(hours - Time.zone.now.hour) + (minutes - Time.zone.now.min)
   end
 
 end
